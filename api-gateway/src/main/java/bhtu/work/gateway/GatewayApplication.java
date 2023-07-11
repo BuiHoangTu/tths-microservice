ab01d5b5991a60a2f3ff2bbdee36b9b922a80717
tu <bhtu.work@gmail.com>
2023-07-11 17:04:08 +0700

    adding gateway

---
+++ api-gateway/src/main/java/bhtu/work/gateway/GatewayApplication.java
@@ -0,0 +1,10 @@
+package bhtu.work.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ discovery-server/src/main/java/bhtu/work/App.java
@@ -0,0 +1,13 @@
+package bhtu.work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
