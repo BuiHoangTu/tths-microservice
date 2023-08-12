2752660f2b6dec79dcc495f38bd94d997ef74b4b
Bùi Hoàng Tú <bhtu.work@gmail.com>
2023-08-12 22:50:18 +0700

    remove dependencies conflics, remove grpc secure

--- auth-service/src/main/java/bhtu/work/tths/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/bhtu/work/tths/authservice/AuthServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerSecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {GrpcServerSecurityAutoConfiguration.class})
 @EnableEurekaClient
 public class AuthServiceApplication {
     public static void main(String[] args) {
