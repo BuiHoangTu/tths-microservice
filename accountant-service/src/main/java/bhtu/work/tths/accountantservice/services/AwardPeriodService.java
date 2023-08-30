806c276e31946c81a44fc7574cd3594ca808ba1f
tu <bhtu.work@gmail.com>
2023-08-30 17:34:35 +0700

    fix date parse always throws exception

--- accountant-service/src/main/java/bhtu/work/tths/accountantservice/services/AwardPeriodService.java
+++ accountant-service/src/main/java/bhtu/work/tths/accountantservice/services/AwardPeriodService.java
@@ -2,14 +2,19 @@ package bhtu.work.tths.accountantservice.services;
 
 import bhtu.work.tths.accountantservice.models.AwardPeriod;
 import bhtu.work.tths.accountantservice.repositories.mongo.AwardPeriodRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 @Service
 public class AwardPeriodService {
+    private static final Logger APS_LOGGER = LoggerFactory.getLogger(AwardPeriodService.class);
+
     private final AwardPeriodRepo awardPeriodRepo;
 
     @Autowired
@@ -21,8 +26,9 @@ public class AwardPeriodService {
         LocalDate dateOfApply;
         if (dateString != null) {
             try {
-                dateOfApply = LocalDate.parse(dateString);
+                dateOfApply = LocalDate.parse(dateString, DateTimeFormatter.ISO_DATE_TIME);
             } catch (DateTimeParseException _e) {
+                APS_LOGGER.info("Can't parse date, choosing now.", _e);
                 dateOfApply = LocalDate.now();
             }
         } else {
